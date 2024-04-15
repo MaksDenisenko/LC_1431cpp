07136538536c500fe6c99f2134ad30b1bef2a77c
Max Denisenko <maksim.denisenko.20@mail.ru>
2024-04-15 16:11:06 +0300

    Добавьте файлы проекта.

---
+++ LC_1431cpp/LC_1431cpp.cpp
@@ -0,0 +1,29 @@
+﻿#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+
+vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+    vector<int> arr(candies.begin(), candies.end());
+    vector<bool> result(candies.size());
+    sort(arr.begin(), arr.end());
+    int numMin = arr.back() - extraCandies;
+    for (int i = 0; i < candies.size(); i++)
+    {
+        if (candies[i] < numMin)
+        {
+            result[i] = false;
+        }
+        else
+            result[i] = true;
+    }
+    return result;
+}
+int main()
+{
+    vector<int> dr = { 2,3,5,1,3 };
+    vector<bool> a = kidsWithCandies(dr, 3);
+    
+}
